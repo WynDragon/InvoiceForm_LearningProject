24bb21603754fc626cb715f9b239f9eaac221ddb
Wyn GARDNER <meagan.gardner@biofiredx.com>
2023-04-27 10:54:05 -0600

    Added To Do for the Search window

--- InvoiceForm/Search.xaml.cs
+++ InvoiceForm/Search.xaml.cs
@@ -19,9 +19,20 @@ namespace InvoiceForm
     /// </summary>
     public partial class Search : Window
     {
+        /// <summary>
+        /// Initializes the Search Window
+        /// </summary>
         public Search()
         {
             InitializeComponent();
         }
+
+        //TO DO:
+        //Set up combo box for search parameters
+        //Search parameters should be by item parameters:
+        //-Name, Cost, Vendor, ItemType
+        //Set up display to show the results of the database queries
+        //all business logic should be in a separate area.
+        //All Queries should be in a Query Class.
     }
 }
