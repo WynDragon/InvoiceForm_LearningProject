609bf039ee17debc45370e79263ccd1ed24be4f1
Wyn GARDNER <meagan.gardner@biofiredx.com>
2023-04-27 13:52:05 -0600

    Added example code and logic to change which window is displayed

--- InvoiceForm/MainWindow.xaml.cs
+++ InvoiceForm/MainWindow.xaml.cs
@@ -20,6 +20,12 @@ namespace InvoiceForm
     /// </summary>
     public partial class MainWindow : Window
     {
+        //In order to call upon another window, we need window objects
+        ItemLogic itemLogic = new ItemLogic();
+
+        /// <summary>
+        /// Initializes the window upon it being opened
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +40,32 @@ namespace InvoiceForm
         {
             //this button switches which window is active
             //switch to Search Window
+        }
+
+        /// <summary>
+        /// Button to switch to ItemsLogic window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditItemsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //this button switches which window is active
+            //try/catch blocks prevent errors from slipping through and potentially causing
+            //unintended memory leaks; good WPF writing formats dictate every function is wrapped
+            //in a try/catch block
+            try
+            {
+                //This line hides the current window
+                this.Hide();
+                //Create a new instance of ItemLogic window
+                ItemLogic itemLogic = new ItemLogic();
+                //Displays the ItemLogic window
+                itemLogic.ShowDialog();
+            }
+            catch(Exception ex) {
+                //will generate an automated message explaining where the error occurred
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
